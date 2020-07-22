ded5979a7dccf32a5f9c8412b54c009f8cd9d5c1
Gerónimo Vega Puente <gero295@gmail.com>
2020-07-22 10:59:25 -0300

    Excercise 4.4

---
+++ bootcampgl2020/src/main/java/com/globallogic/app/EjerciciosFor.java
@@ -0,0 +1,13 @@
+package com.globallogic.app;
+
+public class EjerciciosFor {
+
+	public static void main(String[] args) {
+		
+		for(int i=1 ; i<=10 ; i++) {
+			System.out.println("Valor iterado: " + i);
+		}
+
+	}
+
+}
