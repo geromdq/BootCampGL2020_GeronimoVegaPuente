621bafd1474c5ceafc0414ca9c4d79a1be736752
Gerónimo Vega Puente <gero295@gmail.com>
2020-07-21 23:44:56 -0300

    excercise 3.4

---
+++ bootcampgl2020/src/main/java/com/globallogic/app/StudentDetails.java
@@ -0,0 +1,9 @@
+package com.globallogic.app;
+
+public class StudentDetails {
+	
+	public void studentAge(int edad) {
+		System.out.println("La edad del estudiante es: " + edad);
+	}
+
+}
---
+++ bootcampgl2020/src/main/java/com/globallogic/app/StudentDetailsMain.java
@@ -0,0 +1,12 @@
+package com.globallogic.app;
+
+public class StudentDetailsMain {
+
+	public static void main(String[] args) {
+	
+		StudentDetails estudiante = new StudentDetails();
+		estudiante.studentAge(6);
+
+	}
+
+}
