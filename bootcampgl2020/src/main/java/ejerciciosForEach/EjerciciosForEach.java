74ba9f4d1320eaf41e78203a77a8bbf7028dacee
Gerónimo Vega Puente <gero295@gmail.com>
2020-07-22 17:46:28 -0300

    Excercise 4.9

---
+++ bootcampgl2020/src/main/java/ejerciciosForEach/EjerciciosForEach.java
@@ -0,0 +1,16 @@
+package ejerciciosForEach;
+
+public class EjerciciosForEach {
+
+	public static void main(String[] args) {
+		/*Ejercicio 4.9*/
+		{
+			Integer[] Array = {1,2,3,4,5,6,7,8,9,10};
+			for(Integer num : Array) {
+				System.out.println("Valor iterado: " + num);
+			}
+		}
+
+	}
+
+}
