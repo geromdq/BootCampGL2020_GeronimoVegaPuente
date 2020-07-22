975cb0afe6fe560bc491c4aea7ca971dcbe67692
Gerónimo Vega Puente <gero295@gmail.com>
2020-07-21 23:56:57 -0300

    Excercise 3.5

---
+++ bootcampgl2020/src/main/java/com/globallogic/app/VariableInstancia.java
@@ -0,0 +1,31 @@
+package com.globallogic.app;
+
+public class VariableInstancia {
+	
+	int engPoints;
+	int mathsPoints;
+	int phyPoints;
+	public int getEngPoints() {
+		return engPoints;
+	}
+	public void setEngPoints(int engPoints) {
+		this.engPoints = engPoints;
+	}
+	public int getMathsPoints() {
+		return mathsPoints;
+	}
+	public void setMathsPoints(int mathsPoints) {
+		this.mathsPoints = mathsPoints;
+	}
+	public int getPhyPoints() {
+		return phyPoints;
+	}
+	public void setPhyPoints(int phyPoints) {
+		this.phyPoints = phyPoints;
+	}
+	
+	
+	
+	
+
+}
