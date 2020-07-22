9b49a6779767687a7dd2a4d633256d76ec036cc3
Gerónimo Vega Puente <gero295@gmail.com>
2020-07-22 11:32:25 -0300

    Excercise 4.7

---
+++ bootcampgl2020/src/main/java/com/globallogic/app/EjerciciosWhile.java
@@ -0,0 +1,17 @@
+package com.globallogic.app;
+
+public class EjerciciosWhile {
+
+	public static void main(String[] args) {
+		
+		/*Ejercicio 4.7*/
+		
+		int i = 0;
+		while(i <= 10) {
+			System.out.println("Valor iterado: " + i);
+			i++;
+		}
+
+	}
+
+}
