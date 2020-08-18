458ae310829d703476905bc4b1c474b9848169d5
Gerónimo Vega Puente <gero295@gmail.com>
2020-08-18 02:00:52 -0300

    Intro Spring + Intro Rest con Springboot

--- Order/src/main/java/com/globallogic/bootcampgl/Order/OrderApplication.java
+++ Order/src/main/java/com/globallogic/bootcampgl/Order/OrderApplication.java
@@ -1,13 +1,29 @@
 package com.globallogic.bootcampgl.Order;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrderApplication {
+public class OrderApplication implements CommandLineRunner {
 
+	
+
+	@Autowired	
+	OrderFactory orderFactory;
+	
 	public static void main(String[] args) {
-		SpringApplication.run(OrderApplication.class, args);
+		SpringApplication.run(OrderApplication.class, args);	
+		
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(orderFactory.order.toString());
+		
+	}
+	
+	
 
 }
